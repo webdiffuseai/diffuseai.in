bedaf145d07935ac688c532ce2176229ef458f31
Sujan Saitej <152066198+sujansaitej@users.noreply.github.com>
2025-01-24 23:38:16 +0530

    `Update next.config.mjs to use explicit type import and add output configuration`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+ output: "export",
+};
 
 export default nextConfig;
