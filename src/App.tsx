6fa2fe4dc33ca8f2ecd723ba3c07ac892f079c38
aathif satthar basha <aathifxx7@gmail.com>
2025-04-16 23:46:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,9 +6,13 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useLayoutEffect } from "react";
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
-import About from "./pages/About";
+import About from "./pages/Achievements";
 import Gallery from "./pages/Gallery";
 import NotFound from "./pages/NotFound";
+
+const Achievements = () => {
+    return <div>Achievements Page</div>;
+};
 
 // Scroll to top when navigating
 const ScrollToTop = () => {
@@ -41,7 +44,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/achievement" element={<Achievements />} />
           <Route path="/gallery" element={<Gallery />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
